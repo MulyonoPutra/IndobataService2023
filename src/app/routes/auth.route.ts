92dc5cfd37e89c1d405de344ed4bc8afa22169d5
Mulyono Putra <mulyonoputra1994@gmail.com>
2023-07-11 05:29:43 +0700

    feat: add zod validation middleware

--- src/app/routes/auth.route.ts
+++ src/app/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { login, register } from '../controllers/auth.controller';
+import { validate } from '../middleware/validate';
+import { loginUserSchema, registerUserSchema } from '../utils/input-validation';
 
 const router = Router();
 
-router.post('/register-admin', register('admin'));
-router.post('/register', register());
-router.post('/login', login);
+router.post('/register-admin', validate(registerUserSchema), register('admin'));
+router.post('/register', validate(registerUserSchema), register());
+router.post('/login', validate(loginUserSchema), login);
 
 export default router;
